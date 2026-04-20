d6893ee62016a0c079adecfddc7ce3cb18457c2d
0.1s Dev <01sdevb@gmail.com>
2026-04-20 07:47:41 -0400

    Update artifacts/api-server/src/routes/index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -3,12 +3,14 @@ import healthRouter from "./health";
 import authRouter from "./auth";
 import scriptsRouter from "./scripts";
 import usersRouter from "./users";
+import commentsRouter from "./comments";
 
 const router: IRouter = Router();
 
 router.use(healthRouter);
 router.use(authRouter);
 router.use(scriptsRouter);
+router.use(commentsRouter);
 router.use(usersRouter);
 
 export default router;
